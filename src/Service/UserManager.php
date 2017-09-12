1d7b0b94c651956603d0cd7cb2f199b9a1100486
Форостяный Александр <bosmerer@gmail.com>
2017-09-12 13:02:17 +0300

    debug

--- src/Service/UserManager.php
+++ src/Service/UserManager.php
@@ -165,10 +165,10 @@ class UserManager
     {
         /** @var User $user */
         $user = $this->entityManager->getRepository(User::class)->findOneByEmail($email);
-        print_r($user);
-        echo $email;
-        if($user && in_array($user->getRoleName(),$roles, true)) {
-            return true;
+        if($user != null) {
+            if(in_array($user->getRoleName(),$roles, true)) {
+                return true;
+            }
         }
         return false;
     }
