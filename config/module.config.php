4e7b4e72b180caa5e7110e2256694c1d05ed21fa
Xapp73 <1post112@gmail.com>
2017-10-03 16:50:35 +0300

    refactoring

--- config/module.config.php
+++ config/module.config.php
@@ -110,7 +110,7 @@ return [
     'service_manager' => [
         'factories' => [
             AuthenticationService::class => Service\Factory\AuthenticationServiceFactory::class,
-            Service\AuthAdapter::class => Service\Factory\AuthAdapterServiceFactory::class,
+            Service\AuthAdapter::class => Service\Factory\AuthAdapterFactory::class,
             Service\AuthManager::class => Service\Factory\AuthManagerFactory::class,
             Service\UserManager::class => Service\Factory\UserManagerFactory::class,
             AuthAdapterService::class => AuthAdapterServiceFactory::class,
